8449f107f088f3cda430b48e52cc7a5f82a95803
Kabir Maniar <kabir.maniar@maniartech.com>
2024-02-04 20:25:03 +0530

    Refactor Sidebar component and add SidebarListItem component

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -6,32 +6,25 @@
       </div>
     </a>
     <hr class="sidebar__hr" />
-    <ul class="side-nav">
-      <li
-        v-for="item in items"
-        :key="item"
-        class="sidebar__item"
-        :class="{ 'sidebar__item--active': item.route_name === activeItem }"
-      >
-        <div @click.prevent="navigateTo(item.link)" href="/" class="sidebar__link">
-          <span class="material-symbols-rounded"> {{ item.icon }}</span>
-          <span class="sidebar__item--text">{{ item.title }}</span>
-        </div>
-      </li>
-    </ul>
+    <div class="side-nav">
+      <SidebarListItem v-for="item in items" :key="item.title" :item="item" />
+    </div>
 
     <hr class="sidebar__hr" />
-    <a href="#playlist-modal" class="btn mb-small"> &plus; Create Playlist </a>
-    <ul class="side-nav">
-      <!-- {% with playlists = current_user.playlists %} {% include 'widgets/playlist_tile.html' %} {%
-      endwith %} -->
-    </ul>
+    <a href="#playlist-modal" class="btn mb-small">
+      <span class="material-symbols-rounded">add</span> Create Playlist
+    </a>
+    <ul class="side-nav"></ul>
   </div>
 </template>
 
 <script>
+import SidebarListItem from './SidebarListItem.vue'
 export default {
   name: 'AppSidebar',
+  components: {
+    SidebarListItem
+  },
   data: () => ({
     drawer: false,
     items: [
@@ -41,17 +34,7 @@ export default {
       { title: 'Upload Track', icon: 'upload', link: '/upload', route_name: 'upload' },
       { title: 'Create Album', icon: 'album', link: '/album/add', route_name: 'album-add' }
     ]
-  }),
-  computed: {
-    activeItem() {
-      return this.$route.name
-    }
-  },
-  methods: {
-    navigateTo(link) {
-      this.$router.push(link)
-    }
-  }
+  })
 }
 </script>
 
@@ -94,52 +77,8 @@ export default {
   border-bottom: 1px solid var(--color-border-light);
 }
 
-.sidebar__item {
-  width: 100%;
-  font-size: 2rem;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  color: white;
-  transition: all 0.1s ease-out;
-  border-radius: 5px;
-  border: 1px solid transparent;
-  padding: 1rem 0;
-
-  margin-bottom: 1rem;
-
-  &:hover {
-    background-color: var(--color-hover);
-    border: 1px solid var(--color-border-light);
-  }
-}
-
-.sidebar__item--active {
-  background-color: var(--color-hover);
-}
-
 .side-nav {
   list-style: none;
   width: 100%;
-}
-
-.sidebar__item--icon {
-  display: inline-block;
-  padding: 0 1rem;
-}
-
-.sidebar__item--text {
-  font-weight: 500;
-}
-
-.sidebar__link {
-  cursor: pointer;
-  text-decoration: none;
-  display: grid;
-  grid-template-columns: 5rem 1fr;
-  color: var(--text-color);
-  width: 100%;
-  padding-left: 1rem;
-  align-items: center;
 }
 </style>
---
+++ src/components/SidebarListItem.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-list-item
+    class="sidebar__item"
+    :class="{ 'sidebar__item--active': item.route_name === activeItem }"
+    link
+    color="primary"
+    @click.prevent="navigateTo(item.link)"
+  >
+    <template v-slot:prepend>
+      <span class="material-symbols-rounded"> {{ item.icon }}</span>
+    </template>
+    <v-list-item-title class="sidebar__item--text"> {{ item.title }}</v-list-item-title>
+    <!-- <div @click.prevent="navigateTo(item.link)" href="/" class="sidebar__link">
+      <span class="sidebar__item--text">{{ item.title }}</span>
+    </div> -->
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  name: 'SidebarListItem',
+  props: {
+    item: Object
+  },
+  computed: {
+    activeItem() {
+      return this.$route.name
+    }
+  },
+  methods: {
+    navigateTo(link) {
+      this.$router.push(link)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.sidebar__item {
+  width: 100%;
+  font-size: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  color: white;
+  transition: all 0.1s ease-out;
+  border-radius: 5px;
+  border: 1px solid transparent;
+  padding: 1rem 1rem;
+
+  margin-bottom: 1rem;
+
+  &:hover {
+    background-color: var(--color-hover);
+    border: 1px solid var(--color-border-light);
+  }
+}
+
+.sidebar__item--active {
+  background-color: var(--color-hover);
+}
+
+.sidebar__item--icon {
+  display: inline-block;
+  padding: 0 1rem;
+}
+
+.sidebar__item--text {
+  font-weight: 500;
+  font-size: 2rem;
+  margin-left: 2rem;
+  line-height: normal;
+}
+
+.sidebar__link {
+  cursor: pointer;
+  text-decoration: none;
+  display: grid;
+  grid-template-columns: 5rem 1fr;
+  color: var(--text-color);
+  width: 100%;
+  padding-left: 1rem;
+  align-items: center;
+}
+</style>
