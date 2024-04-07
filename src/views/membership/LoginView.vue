60fbaf5c78757144ea9a3c78deb9e31db3fc8897
Kabir Maniar <kabir.maniar@maniartech.com>
2024-04-07 22:20:05 +0530

    Fixed incorrect styling of Login and Register Pages

--- src/views/membership/LoginView.vue
+++ src/views/membership/LoginView.vue
@@ -2,7 +2,7 @@
   <div class="membership">
     <div class="side-banner">
       <div class="side-banner__logo">&nbsp;</div>
-      <!-- <h2 class="side-banner__text">AUDIFY</h2> -->
+      <h2 class="side-banner__text">AUDIFY</h2>
     </div>
     <LoginForm />
   </div>
